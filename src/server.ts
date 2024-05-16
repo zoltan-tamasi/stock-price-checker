31fe897eca3706b98ea763cb8ae09af63ed2788c
Zoltan Tamasi <tzoltan84@gmail.com>
2024-05-16 13:06:48 +0200

    Basic integration of finnhub

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,20 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
+const finnhub = require('finnhub');
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Server is running OK");
+const api_key = finnhub.ApiClient.instance.authentications['api_key'];
+api_key.apiKey = process.env.FINNHUB_APIKEY;
+const finnhubClient = new finnhub.DefaultApi();
+
+app.get("/:symbol", (req: Request, res: Response) => {
+  finnhubClient.quote(req.params.symbol, (error: any, data: any, response: any) => {
+    res.send(data);
+  });
 });
 
 app.listen(port, () => {
